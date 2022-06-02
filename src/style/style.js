92ed42142aaabeddecbe9f97b6f632c6de341dc6
semiherd <82077230+semiherd@users.noreply.github.com>
2022-06-02 14:45:11 +0300

    Update style.js

--- src/style/style.js
+++ src/style/style.js
@@ -1,8 +1,5 @@
 import {StyleSheet,Dimensions} from 'react-native';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
-
 export default styles= StyleSheet.create({
     container: {
         flex:1,
